46110858c7a9dffe8857295d3a051e7ab3751de1
jdlewis92 <jonathandavidlewis92@gmail.com>
2014-02-23 18:21:56 -0800

    Changes to DebugText
    Changed DebugText to use a queue for the permText field. The queue will
    hold 15 strings before erasing.

--- Project/FinalProject/FinalProject/DebugText.cs
+++ Project/FinalProject/FinalProject/DebugText.cs
@@ -11,19 +11,24 @@ namespace FinalProject
     {
         public SpriteFont Font { set; get; }
         public Vector2 Position { set; get; }
+        public Vector2 PositionPerm { set; get; }
         public Color TextColor { set; get; }
 
         private static DebugText instance = new DebugText();
         private string text;
         private string permText;
+        private Queue<string> permTextQueue;
+        private static readonly int PERMTEXT_CAPACITY = 15;
         private bool displayText;
 
         private DebugText() 
         {
             Position = new Vector2(0, 0);
+            PositionPerm = new Vector2(GraphicsDeviceManager.DefaultBackBufferWidth / 2, 0);
             TextColor = Color.Black;
             text = "";
             permText = "";
+            permTextQueue = new Queue<string>(PERMTEXT_CAPACITY);
             displayText = true;
         }
 
@@ -44,12 +49,27 @@ namespace FinalProject
 
         public void WritePerm(string s)
         {
-            permText += s;
+            permTextQueue.Enqueue(s);
+            UpdatePermText();
         }
 
         public void WriteLinePerm(string s)
         {
-            permText += s + "\n";
+            permTextQueue.Enqueue(s + "\n");
+            UpdatePermText();
+        }
+
+        public void UpdatePermText()
+        {
+            if(permTextQueue.Count > PERMTEXT_CAPACITY)
+               permTextQueue.Dequeue();
+
+            permText = ""; // clear permText then set it to the text inside of the queue
+
+            foreach(string s in permTextQueue)
+            {
+                permText += s;
+            }
         }
 
         public void Draw(SpriteBatch spriteBatch)
@@ -57,7 +77,7 @@ namespace FinalProject
             if (displayText)
             {
                 spriteBatch.DrawString(Font, text, Position, TextColor);
-                spriteBatch.DrawString(Font, permText, new Vector2(400, 0), TextColor);
+                spriteBatch.DrawString(Font, permText, PositionPerm, TextColor);
             }
             ClearText();    //Clear the text so we don't end up with text going off the bottom of the screen - since everything is updating every tick, this won't cause any information loss
         }
