b507275ec817a3cfc44dad75adbd9c2571726c17
jdlewis92 <jonathandavidlewis92@gmail.com>
2014-03-18 23:23:27 -0700

    Added a well for the game end trigger
    Now in order to end the game, you must make it to the well!

---
+++ Project/FinalProject/FinalProject/GameObjects/Sticker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace FinalProject
+{
+    class Sticker : Drawable
+    {
+        private Texture2D sprite;
+
+        public Vector2 Position { get; set; }
+
+        public Sticker(Texture2D sprite, Vector2 position)
+        {
+            this.sprite = sprite;
+            Position = position;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(sprite, Position, Color.White);
+        }
+    }
+}
